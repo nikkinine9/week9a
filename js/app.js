ff80cdc700502e7829a733028e63ee05446f6825
nikkinine9 <nikkinine75@gmail.com>
2020-11-14 16:28:17 -0700

    week9b simple user array

--- js/app.js
+++ js/app.js
@@ -1,24 +1,13 @@
-var age = 18;
-var is_subscribed = confirm("Do you have a subscription?");
+var age = [5, 8, 10, 16, 20, 30, 40, 50];
+var is_subscribed = [true, true, true, false, false, true, false, true];
 
-if (age < 18 && is_subscribed == false) {
-    console.log(age + " is to young and you are not subscribed to this site!");
-} else if (age >= 18 && is_subscribed == false) {
-    console.log(age + " is permitted but you are not subscribed to this site!");
-} else if (age < 18 && is_subscribed == true) {
-    console.log(age + " is too young but you are subscribed to this site!");
-} else {
-    console.log(age + " is the correct age and you are subscribed to this site!");
-}
-
-
-var counter = 0;
-while (counter < 10) {
-    console.log(counter);
-    counter++;
-}
-
-// for alternative
-for (var counter = 0; counter < 10; counter++) {
-    console.log(counter);
-}
+for (var i = 0; i < age.length; i++) {
+    if (is_subscribed[i] == false && age[i] < 18) {
+        console.log(age + " is to young and you are not subscribed to this site!");
+    } else if (is_subscribed == false && age[i] > 18) {
+        console.log(age + " is permitted but you are not subscribed to this site!");
+    } else if (is_subscribed == true && age[i] < 18) {
+        console.log(age + " is too young but you are subscribed to this site!");
+    } else if (is_subscribed == true && age[i] > 18) {
+        console.log(age + " is the correct age and you are subscribed to this site!");
+    }
