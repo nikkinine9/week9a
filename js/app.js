bc25f3d61fa2fae69c9bb34a9d1ed0e9541ebe42
nikkinine9 <nikkinine75@gmail.com>
2020-11-14 21:24:47 -0700

    w9b arrays and loops

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
-var age = [5, 8, 10, 16, 20, 30, 40, 50];
-var is_subscribed = [false, true, true, false, false, true, false, true];
+var userAges = [16, 21, 12, 30]
+var subscriptionStatus = [true, false, false, true];
 
-for (var i = 0; i < age.length; i++) {
-    if (is_subscribed[i] == false && age[i] < 18) {
+for (var index = 0; index < 4; index++) {
+    var userAge = userAges[index];
+    var subscriptionStatus = subscriptionStatus[index];
+
+    if (subscriptionStatus == false && userAges[index] < 18) {
         console.log("The is to young and you are not subscribed to this site!");
-    } else if (is_subscribed == false && age[i] >= 18) {
+    } else if (subscriptionStatus == false && userAges[index] >= 18) {
         console.log(" The user is permitted but you are not subscribed to this site!");
-    } else if (is_subscribed == true && age[i] < 18) {
+    } else if (subscriptionStatus == true && userAges[index] < 18) {
         console.log(" The user is too young but you are subscribed to this site!");
-    } else(is_subscribed == true && age[i] >= 18); {
+    } else if (subscriptionStatus == true && userAges[index] >= 18); {
         console.log(" The user is the correct age and you are subscribed to this site!");
     }
 }
