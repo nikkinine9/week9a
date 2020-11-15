f767c3232cddb1215c33e01b054ca5352244e6cf
nikkinine9 <nikkinine75@gmail.com>
2020-11-14 18:34:27 -0700

    week9b simple user array

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ var is_subscribed = [false, true, true, false, false, true, false, true];
 
 for (var i = 0; i < age.length; i++) {
     if (is_subscribed[i] == false && age[i] < 18) {
-        console.log(age + " is to young and you are not subscribed to this site!");
-    } else if (is_subscribed == false && age[i] > 18) {
-        console.log(age + " is permitted but you are not subscribed to this site!");
+        console.log("The is to young and you are not subscribed to this site!");
+    } else if (is_subscribed == false && age[i] >= 18) {
+        console.log(" The user is permitted but you are not subscribed to this site!");
     } else if (is_subscribed == true && age[i] < 18) {
-        console.log(age + " is too young but you are subscribed to this site!");
-    } else if (is_subscribed == true && age[i] > 18) {
-        console.log(age + " is the correct age and you are subscribed to this site!");
-    } else console.log("uh oh"); {}
+        console.log(" The user is too young but you are subscribed to this site!");
+    } else(is_subscribed == true && age[i] >= 18); {
+        console.log(" The user is the correct age and you are subscribed to this site!");
+    }
 }
